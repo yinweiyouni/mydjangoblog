527c46500c2782635408158a9e3cc09781b5e40f
YE <1710492709@qq.com>
2017-10-10 00:08:11 +0800

    Update settings.py
    change debug to false  and add static_root

--- djangoblog/djangoblog/settings.py
+++ djangoblog/djangoblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^@0_+hnww-wr^mo#f(ttva=%eftq6juv3#7o#0jo%k1kjo9j%s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','.yinweiyouni.site']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/blog/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
